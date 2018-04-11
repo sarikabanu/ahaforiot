bee349140700cf6a62921c5c3b3467f071df3be8
sarikabanu <sarika.b@shrigowri.com>
2018-04-11 13:40:01 +0530

    Update development.js

--- config/development.js
+++ config/development.js
@@ -1,12 +1,1 @@
- module.exports = {
 
-    mysqlConnection: {
-        host:'musicappservernew.mysql.database.azure.com',
-        user:'musicapp@musicappservernew',
-        password: 'music12!@',
-        database: 'iotdb',
-        port: 3306,
-        ssl: true
-  }
-  
-}
